f101d2f211717ed853ed8b44ec49a4108dc3419d
Syam <99113580+syamkarthiks@users.noreply.github.com>
2026-02-03 00:43:44 +0530

    Update visitors.js

--- api/visitors.js
+++ api/visitors.js
@@ -15,6 +15,7 @@ export default async function handler(req, res) {
 
     const data = await response.json();
     res.status(200).json(data);
+
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
