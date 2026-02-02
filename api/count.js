9d18b904a84ae4afaf06244e847d6bbf41cfcc34
Syam <99113580+syamkarthiks@users.noreply.github.com>
2026-02-03 00:44:50 +0530

    Update count.js

--- api/count.js
+++ api/count.js
@@ -1,7 +1,6 @@
 export default async function handler(req, res) {
   const SUPABASE_URL = "https://kjyzjgxusouthqxrebdt.supabase.co";
   const SUPABASE_KEY = "sb_publishable_sUEv6TQ39NRzaafzTPGpMQ_zufoSyMX";
-
   try {
     const response = await fetch(
       `${SUPABASE_URL}/rest/v1/visitors?select=id`,
@@ -15,6 +14,7 @@ export default async function handler(req, res) {
 
     const data = await response.json();
     res.status(200).json({ total: data.length });
+
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
