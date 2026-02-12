2a77518a4281b1fe29edef45746d7443b7dd09ae
Syam <99113580+syamkarthiks@users.noreply.github.com>
2026-02-12 13:49:43 +0530

    Update visitors.js

--- api/visitors.js
+++ api/visitors.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+
   const SUPABASE_URL = "https://vjjtljbsyoeszyayunzc.supabase.co";
   const SUPABASE_KEY = "sb_publishable_f9MIKH_F9LHTUePZ6D5lYg_0zQob5vX";
 
